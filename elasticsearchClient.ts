160b8fef6576ba0e08297b51a5b81e6ef29d474f
Jussiles <juss.leskinen@gmail.com>
2021-08-11 10:44:05 +0300

    Add auth.

--- elasticsearchClient.ts
+++ elasticsearchClient.ts
@@ -3,5 +3,11 @@ import { Client } from "@elastic/elasticsearch";
 export const getClient = (): Client => {
   const url = process.env.ELASTICSEARCH_URL;
   if (!url) throw "Set ELASTICSEARCH_URL";
-  return new Client({ node: process.env.ELASTICSEARCH_URL });
+  return new Client({ 
+    node: url,
+    auth: {
+      // @ts-expect-error
+      bearer: process.env.SECRET_KEY,
+    }
+  });
 }
